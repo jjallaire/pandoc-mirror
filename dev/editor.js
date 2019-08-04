651d05ab5a67c5744450a76a1e8d9dcce97a7ecc
jjallaire <jj@rstudio.com>
2019-08-04 11:14:41 -0400

    host editor in w2ui

--- dev/editor.js
+++ dev/editor.js
@@ -39,19 +39,44 @@ let pandoc = {
   }
 };
 
-// create editor
-let editor = new PandocMirror.Editor({
-  parent: document.getElementById('editor'), 
-  pandoc,
-  ui,
-  options: {
-    autoFocus: true
-  },
-  hooks
-});
 
-// get content and load it into the editor
-axios.get('content.md') .then(result => {
-  editor.setContent(result.data)
-})
+// function to initialize editor given a parent
+function initEditor(parent) {
+  // create editor
+  let editor = new PandocMirror.Editor({
+    parent, 
+    pandoc,
+    ui,
+    options: {
+      autoFocus: true
+    },
+    hooks
+  });
+
+  // get content and load it into the editor
+  axios.get('content.md') .then(result => {
+    editor.setContent(result.data)
+  })
+}
+
+$('#layout').w2layout({
+  name: 'layout',
+  panels: [{ 
+    type: 'main', 
+    overflow: 'scroll',
+    toolbar: {
+      items: [
+        { type: 'button',  id: 'item5',  caption: 'Item 5', icon: 'w2ui-icon-check', hint: 'Hint for item 5' }
+      ]
+    }, 
+    content: {
+      render: function() {
+        const container = $("<div id='container'></div>")
+        $(this.box).append(container);
+        initEditor(container.get(0))
+      }
+    }
+  },
+  ]
+});
 
