246ee2330c9a52622b3ff5542d9d370ba80d42f4
jjallaire <jj@rstudio.com>
2019-08-28 13:08:39 -0400

    apply smarty transforms to pasted plain text

--- src/behaviors/smarty.ts
+++ src/behaviors/smarty.ts
@@ -1,11 +1,51 @@
-import { ellipsis, emDash } from 'prosemirror-inputrules';
+import { smartQuotes, ellipsis, emDash } from 'prosemirror-inputrules';
 
 import { Extension } from 'api/extension';
+import { Plugin, PluginKey } from 'prosemirror-state';
+import { Schema } from 'prosemirror-model';
+
+const plugin = new PluginKey('smartypaste');
 
 const extension: Extension = {
   inputRules: () => {
-    return [ellipsis, emDash];
+    return [...smartQuotes, ellipsis, emDash];
   },
+
+  plugins: (schema: Schema) => {
+    return [
+
+      // apply smarty rules to plain text pastes
+      new Plugin({
+        key: plugin,
+        props: {
+          transformPastedText(text: string) {
+
+            // double quotes
+            text = text.replace(/(?:^|[\s\{\[\(\<'"\u2018\u201C])(")/g, x => {
+              return x.slice(0, x.length-1) + "“";
+            });
+            text = text.replace(/"/g, "”");
+
+            // single quotes
+            text = text.replace(/(?:^|[\s\{\[\(\<'"\u2018\u201C])(')/g, x => {
+              return x.slice(0, x.length-1) + "‘";
+            });
+            text = text.replace(/'/g, "’");
+
+            // emdash
+            text = text.replace(/--/, "—");
+
+            // ellipses
+            text = text.replace(/\.\.\./, "…");
+
+            return text;
+          }
+        }
+      })
+    ];
+  }
 };
+
+
 
 export default extension;
