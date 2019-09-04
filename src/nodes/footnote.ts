1bdc3bcc1c7830be713f90da03fefdf3b6c0c329
jjallaire <jj@rstudio.com>
2019-09-04 12:27:06 -0400

    selection behavior for foonotes

--- src/nodes/footnote.ts
+++ src/nodes/footnote.ts
@@ -1,13 +1,14 @@
-import { Node as ProsemirrorNode, Schema, Fragment } from 'prosemirror-model';
-import { Plugin, PluginKey, EditorState, Transaction } from 'prosemirror-state';
+import { Node as ProsemirrorNode, Schema, Fragment, ResolvedPos } from 'prosemirror-model';
+import { Plugin, PluginKey, EditorState, Transaction, Selection, TextSelection } from 'prosemirror-state';
 import { findChildrenByType, findSelectedNodeOfType, findParentNodeOfType, ContentNodeWithPos, NodeWithPos, findChildren } from 'prosemirror-utils';
-import { DecorationSet } from 'prosemirror-view';
+import { DecorationSet, NodeView, EditorView, Decoration } from 'prosemirror-view';
 
 import { Extension } from 'api/extension';
 import { PandocOutput } from 'api/pandoc';
 import { createNoteId } from 'api/notes';
 import { nodeDecoration } from 'api/decoration';
 import { transactionsHaveChange } from 'api/transaction';
+import { Editor } from 'editor';
 
 
 const plugin = new PluginKey('footnote');
@@ -90,14 +91,14 @@ const extension: Extension = {
 
               // resolve the specific footnote node or specific note node
               if (footnoteNode) {
-                const ref = (footnoteNode as NodeWithPos).node.attrs.ref;
+                const ref = footnoteNode.node.attrs.ref;
                 const matching = findChildren(notesNode.node, node => node.attrs.id === ref);
                 if (matching.length) {
                   noteNode = matching[0];
                   noteNode.pos = notesNode.pos + 1 + noteNode.pos; 
                 }
               } else if (noteNode) {
-                const id = (noteNode as NodeWithPos).node.attrs.id;
+                const id = noteNode.node.attrs.id;
                 const matching = findChildren(state.doc, node => node.type === schema.nodes.footnote && node.attrs.ref === id, true);
                 if (matching.length) {
                   footnoteNode = matching[0];
@@ -108,6 +109,8 @@ const extension: Extension = {
                 return DecorationSet.create(state.doc, [
                   // make notes node visible
                   nodeDecoration(noteNode, { class: 'active' }),
+
+                  // paint outline over footnote
                   nodeDecoration(footnoteNode, { class: 'active' }),
 
                   // position body and notes nodes for footnote editing
@@ -120,97 +123,149 @@ const extension: Extension = {
             } else {
               return DecorationSet.empty;
             }
-          }
+          },
+
+          nodeViews: {
+            note(node, view, getPos) {
+              return new NoteView(node, view, getPos);
+            },
+          },
         },
 
         appendTransaction: (transactions: Transaction[], oldState: EditorState, newState: EditorState) => {
 
-          // bail if there were no changes affecting footnotes/notes
-          const footnoteChange = (node: ProsemirrorNode) => node.type === schema.nodes.footnote || node.type === schema.nodes.note;
-          if (!transactionsHaveChange(transactions, oldState, newState, footnoteChange)) {
-            return;
-          }
-
           // transaction
           const tr = newState.tr;
 
-          // footnotes in the document
-          const footnotes = findChildrenByType(newState.doc, schema.nodes.footnote, true);
+          // do footnote fixups if there were any changes affecting footnotes
+          const footnoteChange = (node: ProsemirrorNode) => node.type === schema.nodes.footnote || node.type === schema.nodes.note;
+          if (transactionsHaveChange(transactions, oldState, newState, footnoteChange)) {
+           
+            // footnotes in the document
+            const footnotes = findChildrenByType(newState.doc, schema.nodes.footnote, true);
 
-          // notes container
-          const notes = findChildrenByType(newState.doc, schema.nodes.notes, true)[0];
+            // notes container
+            const notes = findChildrenByType(newState.doc, schema.nodes.notes, true)[0];
 
-          // iterate through footnotes in the newState
-          const refs = new Set<string>();
-          footnotes.forEach((footnote, index) => {
+            // iterate through footnotes in the newState
+            const refs = new Set<string>();
+            footnotes.forEach((footnote, index) => {
 
-            // alias ref and content (either or both may be updated)
-            let { ref, content } = footnote.node.attrs;
-            
-            // we may be creating a new note to append
-            let newNote: any;
+              // alias ref and content (either or both may be updated)
+              let { ref, content } = footnote.node.attrs;
+              
+              // we may be creating a new note to append
+              let newNote: any;
 
-            // get reference to note (if any)
-            const note = findChildrenByType(newState.doc, schema.nodes.note, true).find(
-              noteWithPos => noteWithPos.node.attrs.id === ref,
-            );
-
-            // matching note found 
-            if (note) {
-
-              // update content if this particular note changed
-              // (used to propagate user edits back to data-content)
-              if (transactionsHaveChange(
-                  transactions, oldState, newState,
-                  node => node.type === schema.nodes.note && node.attrs.id === ref)
-                 ) {
-                content = JSON.stringify(note.node.content.toJSON());
-              }
-
-              // if we've already processed this ref then it's a duplicate, make a copy w/ a new ref/id
-              if (refs.has(ref)) {
-
-                // create a new unique id and change the ref to it
-                ref = createNoteId();
-
-                // create and insert new note with this id
-                newNote = schema.nodes.note.createAndFill({ id: ref }, note.node.content);
-              }
-
-            // if there is no note then create one using the content attr
-            } else {
-              newNote = schema.nodes.note.createAndFill(
-                { id: ref },
-                Fragment.fromJSON(schema, JSON.parse(content)),
+              // get reference to note (if any)
+              const note = findChildrenByType(newState.doc, schema.nodes.note, true).find(
+                noteWithPos => noteWithPos.node.attrs.id === ref,
               );
-            }
 
-            // insert newNote if necessary
-            if (newNote) {
-              tr.insert(notes.pos + 1, newNote as ProsemirrorNode);
-            }
+              // matching note found 
+              if (note) {
 
-            // indicate that we've seen this ref
-            refs.add(ref);
+                // update content if this particular note changed
+                // (used to propagate user edits back to data-content)
+                if (transactionsHaveChange(
+                    transactions, oldState, newState,
+                    node => node.type === schema.nodes.note && node.attrs.id === ref)
+                  ) {
+                  content = JSON.stringify(note.node.content.toJSON());
+                }
 
-            // set new footnote markup
-            tr.setNodeMarkup(footnote.pos, schema.nodes.footnote, {
-              ...footnote.node.attrs,
-              ref,
-              content,
-              number: index + 1,
+                // if we've already processed this ref then it's a duplicate, make a copy w/ a new ref/id
+                if (refs.has(ref)) {
+
+                  // create a new unique id and change the ref to it
+                  ref = createNoteId();
+
+                  // create and insert new note with this id
+                  newNote = schema.nodes.note.createAndFill({ id: ref }, note.node.content);
+                }
+
+              // if there is no note then create one using the content attr
+              } else {
+                newNote = schema.nodes.note.createAndFill(
+                  { id: ref },
+                  Fragment.fromJSON(schema, JSON.parse(content)),
+                );
+              }
+
+              // insert newNote if necessary
+              if (newNote) {
+                tr.insert(notes.pos + 1, newNote as ProsemirrorNode);
+              }
+
+              // indicate that we've seen this ref
+              refs.add(ref);
+
+              // set new footnote markup
+              tr.setNodeMarkup(footnote.pos, schema.nodes.footnote, {
+                ...footnote.node.attrs,
+                ref,
+                content,
+                number: index + 1,
+              });
             });
-          });
+          }
 
-          if (tr.docChanged) {
+          // if a footnote is selected then forward selection to it's corresponding note editor
+          const footnoteNode: NodeWithPos | undefined = findSelectedNodeOfType(schema.nodes.footnote)(newState.selection);
+          if (footnoteNode) {
+            const ref = footnoteNode.node.attrs.ref;
+            const noteNode = findChildren(newState.doc, node => node.type === schema.nodes.note && node.attrs.id === ref, true);
+            if (noteNode.length) {
+              tr.setSelection(TextSelection.near(newState.doc.resolve(noteNode[0].pos)));
+            }
+          }
+
+          if (tr.docChanged || tr.selectionSet) {
             return tr;
           }
+          
         }
-      
+
       }),
     ];
   },
 };
+
+class NoteView implements NodeView {
+  public readonly dom: HTMLElement;
+  public readonly contentDOM: HTMLElement;
+
+  private readonly node: ProsemirrorNode;
+  private readonly view: EditorView;
+  private readonly getPos: () => number;
+
+  constructor(node: ProsemirrorNode, view: EditorView, getPos: () => number) {
+    this.node = node;
+    this.view = view;
+    this.getPos = getPos;
+
+    // ['div', { id: node.attrs.id, class: 'note' }, 0]
+
+    // create note
+    this.dom = window.document.createElement('div');
+    this.dom.id = this.node.attrs.id;
+    this.dom.classList.add('note');
+    this.contentDOM = this.dom;
+
+  }
+
+  /*
+  public update(_node: ProsemirrorNode, decorations: Decoration[]) {
+    if (decorations.length) {
+      const state = this.view.state;
+      
+      this.view.dispatch(state.tr.setSelection(TextSelection.near(state.doc.resolve(this.getPos()))));
+    }
+    // handle change of decorations (e.g. class: 'active' => inactive)
+    return true;
+  }
+  */
+}
 
 /*
 
