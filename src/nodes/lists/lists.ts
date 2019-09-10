c435339f6262bfbc93fcae7256972b7bfac8869c
jjallaire <jj@rstudio.com>
2019-09-10 14:58:23 -0400

    tab/shift-tab for list items

--- src/nodes/lists/lists.ts
+++ src/nodes/lists/lists.ts
@@ -232,7 +232,9 @@ const extension: Extension = {
       'Shift-Ctrl-9': wrapInList(schema.nodes.ordered_list),
       Enter: splitListItem(schema.nodes.list_item),
       'Mod-[': liftListItem(schema.nodes.list_item),
+      'Shift-Tab': liftListItem(schema.nodes.list_item),
       'Mod-]': sinkListItem(schema.nodes.list_item),
+      'Tab': sinkListItem(schema.nodes.list_item)
     };
   },
 
