a97d0d0f195780c31e9ce41711fe2d2caa8108d2
jjallaire <jj@rstudio.com>
2019-09-12 10:44:57 -0400

    horizontal_rule: input rule and a bit more margin

--- src/nodes/horizontal_rule.ts
+++ src/nodes/horizontal_rule.ts
@@ -3,6 +3,9 @@ import { Schema } from 'prosemirror-model';
 import { Command, insertNode } from 'api/command';
 import { Extension } from 'api/extension';
 import { PandocOutput } from 'api/pandoc';
+import { InputRule } from 'prosemirror-inputrules';
+import { findParentNodeOfType } from 'prosemirror-utils';
+import { EditorState } from 'prosemirror-state';
 
 const extension: Extension = {
   nodes: [
@@ -31,6 +34,20 @@ const extension: Extension = {
 
   commands: (schema: Schema) => {
     return [new Command('horizontal_rule', ['Mod-_'], insertNode(schema.nodes.horizontal_rule))];
+  },
+
+  inputRules: (schema: Schema) => {
+
+    return [
+      new InputRule(/^\*{3}$/, (state: EditorState, match: string[], start: number, end: number) => {
+        const paraNode = findParentNodeOfType(schema.nodes.paragraph)(state.selection);
+        if (paraNode && state.selection.$anchor.depth === 2) { // only in top-level paragraphs
+          return state.tr.replaceRangeWith(start, end, schema.nodes.horizontal_rule.create());
+        } else {
+          return null;
+        }
+      })
+    ];
   },
 };
 
